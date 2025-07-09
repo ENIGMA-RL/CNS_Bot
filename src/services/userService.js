8b8f08fb3cb7058a1709b56e3cf73356bb6193ab
ENIGMA <dmgincenigma@gmail.com>
2025-07-09 11:30:46 +0200

    Token env

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,7 +4,7 @@ async function getUserData(userId) {
   try {
     const response = await fetch(`https://discord.com/api/v10/users/${userId}`, {
       headers: {
-        Authorization: `Bot ${process.env.PROD_TOKEN}`,
+        Authorization: `Bot ${process.env.TOKEN}`,
       },
       timeout: 10000,
     });
